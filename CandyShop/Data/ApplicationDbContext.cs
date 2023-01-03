a418237f88193fc772085ca652c1357404aa1112
DevSeif <65452750+DevSeif@users.noreply.github.com>
2023-01-03 21:30:50 +0100

    Add files via upload

---
+++ CandyShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CandyShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CandyShop.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+    }
+}
